49fea3293225f0e616589a77da134df2588ae54b
hegaoye@qq.com <hegaoye@qq.com>
2020-04-26 15:06:55 +0800

    删除无用文件

--- src/wechat/contact.py
+++
@@ -1,8 +0,0 @@
-class Contact:
-    """
-    联系人通讯录信息封装
-    """
-
-    def __init__(self, name, logo=None):
-        self.name = name
-        self.logo = logo
--- src/wechat/group.py
+++
@@ -1,8 +0,0 @@
-class Group:
-    """
-    微信群
-    """
-
-    def __init__(self, name, logo=None):
-        self.logo = logo
-        self.name = name
