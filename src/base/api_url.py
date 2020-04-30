ba3c088c33bae3ba4fe318e61baefe698ef0e104
hegaoye@qq.com <hegaoye@qq.com>
2020-04-30 10:50:58 +0800

    增加枚举类将请求接口提炼出来

--- src/base/api_url.py
+++ src/base/api_url.py
@@ -5,22 +5,22 @@ class Api(Enum):
     Host = "http://www.rzhkj.com/"
 
     """
-    微信信息接口
+    微信信息接口 post 推送获取的通讯录，群聊列表，微信个人信息
     """
     Wechat_Info_Url = Host + "/wechat/info"
 
     """
-    获取任务
+    获取任务 get 根据ip，序列号获取当前设备的任务
     """
     Get_Task_Url = Host + "xxx.shtml?serialno={serialno}&ip={ip}"
 
     """
-    获取新设备
+    获取新设备 get 获取可能上线的新设备列表
     """
     Device_New_Url = Host + "device/new"
 
     """
-    设备上线
+    设备上线 post 设备成功上线后将会通知云端，云端对设备状态进行修改
     """
     Device_Online_Url = Host + "device/online"
 
